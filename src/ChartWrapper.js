56e0601e8b254f4ce2a6ef68283509b910233804
Keisuke Miura <keisuke@g.ecc.u-tokyo.ac.jp>
2023-08-29 11:20:52 +0900

    format source codes with Prettier

--- src/ChartWrapper.js
+++ src/ChartWrapper.js
@@ -1,19 +1,17 @@
-import React, { useRef, useState, useEffect } from 'react';
-import D3Chart from './D3Chart';
+import React, { useRef, useState, useEffect } from "react";
+import D3Chart from "./D3Chart";
 
 const ChartWrapper = () => {
-	const chartArea = useRef(null)
-	const [chart, setChart] = useState(null)
+  const chartArea = useRef(null);
+  const [chart, setChart] = useState(null);
 
-	useEffect(() => {
-		if (!chart) {
-			setChart(new D3Chart(chartArea.current))
-		}
-	}, [chart])
+  useEffect(() => {
+    if (!chart) {
+      setChart(new D3Chart(chartArea.current));
+    }
+  }, [chart]);
 
-	return (
-		<div ref={chartArea}></div>
-	)
-}
+  return <div ref={chartArea}></div>;
+};
 
-export default ChartWrapper
+export default ChartWrapper;
